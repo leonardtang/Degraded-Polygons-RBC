16f151ae2cb117fc7ecd8d610ca7e57b0ff78a0b
Leonard Tang <leonardtang@balrog.Berkeley.EDU>
2022-04-19 22:27:11 -0700

    First commit

---
+++ explore.py
@@ -0,0 +1,5 @@
+import cv2
+
+img = cv2.imread("./data/png/alarm clock/81.png")
+print(img.shape)
+# Dims are (1111, 1111, 3)
---
+++ recover.py
@@ -0,0 +1,10 @@
+"""
+Construct non-recoverable images in the style of the Gollin figure test.
+"""
+
+def remove_corners():
+    pass 
+
+
+def remove_midedge():
+    pass
